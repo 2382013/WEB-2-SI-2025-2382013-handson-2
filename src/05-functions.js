53196599f90b7865b5b0abb97a57b2ee687a46b6
yessica <2382013@unai.edu>
2025-01-22 13:30:26 +0700

    second commit

--- src/05-functions.js
+++ src/05-functions.js
@@ -1,41 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // Basic function dengan tipe
-function add(a: number, b: number): number{
+function add(a, b) {
     return a + b;
 }
-
 // Arrow function dengan tipe
-const multiply = (x: number, y: number): number => {
+const multiply = (x, y) => {
     return x * y;
 };
-
 // Function dengan optional parameter
-function greet(name: string, greeting?: string): string {
+function greet(name, greeting) {
     if (greeting) {
         return `${greeting}, ${name}!`;
     }
     return `Hello, ${name}!`;
 }
-
 // Function dengan default parameter
-function createEmail(to: string, subject: string = "No Subject"): string {
+function createEmail(to, subject = "No Subject") {
     return `Email to : ${to}, ${subject}`;
 }
-
 // Function dengan rest parameters
-function sum(...numbers: number[]): number {
-    return numbers.reduce((total, num) => total +num, 0);
+function sum(...numbers) {
+    return numbers.reduce((total, num) => total + num, 0);
 }
-
-// Fuction overloads
-function processInput(input: number): number;
-function processInput(input: string): string;
-function processInput(input: number | string): number | string {
+function processInput(input) {
     if (typeof input === "number") {
         return input * 2;
     }
     return input.toUpperCase();
 }
-
 //console.log(add(10, 5));
 //console.log(multiply(10, 5));
 //console.log(greet("John"));
@@ -43,6 +36,3 @@ function processInput(input: number | string): number | string {
 //console.log(createEmail("john@example.com"));
 //console.log(sum(1, 2, 3, 4, 5));
 console.log(processInput("Hello World"));
-
-
-export {};
